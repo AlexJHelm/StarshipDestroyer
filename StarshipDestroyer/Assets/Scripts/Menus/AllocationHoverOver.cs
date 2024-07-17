359d747515226ce53e1ce8da9dd34103e26439df
Zurius <jacobsouza32@gmail.com>
2024-07-17 16:38:28 -0400

    Menu Overhaul Merge

--- StarshipDestroyer/Assets/Scripts/Menus/AllocationHoverOver.cs
+++ StarshipDestroyer/Assets/Scripts/Menus/AllocationHoverOver.cs
@@ -8,12 +8,15 @@ public class AllocationHoverOver : MonoBehaviour, IPointerEnterHandler
     public GameObject HoverPanel1;
     public GameObject HoverPanel2;
     public GameObject HoverPanel3;
+    public Vector3 panelPosition;
+    public Vector3 inFocusPosition = new Vector3(311, 215, 0);
+    public Vector3 outOfFocusPosition = new Vector3(-1000, -1000, 0);
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        HoverPanel1.SetActive(false);
-        HoverPanel2.SetActive(true);
-        HoverPanel3.SetActive(false);
+        HoverPanel1.transform.position = outOfFocusPosition;
+        HoverPanel2.transform.position = inFocusPosition;
+        HoverPanel3.transform.position = outOfFocusPosition;
 
     }
 }
--- StarshipDestroyer/Assets/Scripts/Menus/PlayerHoverOver.cs
+++ StarshipDestroyer/Assets/Scripts/Menus/PlayerHoverOver.cs
@@ -8,12 +8,15 @@ public class PlayerHoverOver : MonoBehaviour, IPointerEnterHandler
     public GameObject HoverPanel1;
     public GameObject HoverPanel2;
     public GameObject HoverPanel3;
+    public Vector3 panelPosition;
+    public Vector3 inFocusPosition = new Vector3(311, 215, 0);
+    public Vector3 outOfFocusPosition = new Vector3(-1000, -1000, 0);
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        HoverPanel1.SetActive(false);
-        HoverPanel2.SetActive(false);
-        HoverPanel3.SetActive(true);
+        HoverPanel1.transform.position = outOfFocusPosition;
+        HoverPanel2.transform.position = outOfFocusPosition;
+        HoverPanel3.transform.position = inFocusPosition;
 
     }
 }

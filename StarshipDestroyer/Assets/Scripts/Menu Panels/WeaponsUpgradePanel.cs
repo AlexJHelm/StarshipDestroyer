749f609c78f214b6cc72a63755d09f1b009efa29
Zurius <jacobsouza32@gmail.com>
2024-08-25 17:33:45 -0400

    Menu Changes

---
+++ StarshipDestroyer/Assets/Scripts/Menu Panels/WeaponsUpgradePanel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WeaponsUpgradePane : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject HoverPanel1;
+    public GameObject HoverPanel2;
+    public GameObject HoverPanel3;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HoverPanel1.SetActive(false);
+        HoverPanel2.SetActive(true);
+        HoverPanel3.SetActive(false);
+
+    }
+}

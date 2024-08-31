be3fe1d005928d46d65b3969d7a3497692d9fa21
Zurius <jacobsouza32@gmail.com>
2024-08-31 17:47:56 -0400

    Pause Menu

--- StarshipDestroyer/Assets/Scripts/Utilities/GameManager.cs
+++ StarshipDestroyer/Assets/Scripts/Utilities/GameManager.cs
@@ -351,6 +351,8 @@ public class GameManager : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = false;
+            scrapText = GameObject.FindWithTag("ScrapCount").GetComponent<TMP_Text>();
+            scrapText.text = $"{scrap}";
         }
         else
         {

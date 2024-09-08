909cf48be274a28689eba4a15612d8f38b79ddef
Zurius <jacobsouza32@gmail.com>
2024-09-08 07:00:52 -0400

    fullscreen mode

--- StarshipDestroyer/Assets/Scripts/Settings/ScreenMode.cs
+++ StarshipDestroyer/Assets/Scripts/Settings/ScreenMode.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;  // Import UI to access Text and Button components
 
 public class ScreenMode : MonoBehaviour
 {
-    public void SetFullscreen (bool isFullscreen)
+    public TMP_Text buttonText;  // Reference to the button text
+
+    // Method to toggle fullscreen and update the button text
+    public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        UpdateButtonText(isFullscreen);
+    }
+
+    // Method to update button text based on the fullscreen mode
+    private void UpdateButtonText(bool isFullscreen)
+    {
+        if (isFullscreen)
+        {
+            buttonText.text = "Full Screen";
+        }
+        else
+        {
+            buttonText.text = "Windowed";
+        }
+    }
+
+    // Optional: Automatically update button text based on initial screen mode
+    private void Start()
+    {
+        UpdateButtonText(Screen.fullScreen);
     }
 }

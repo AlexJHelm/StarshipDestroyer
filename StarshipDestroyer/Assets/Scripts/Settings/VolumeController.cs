b76def25f4149a7a36b9710f54f9a33340034f6c
Zurius <jacobsouza32@gmail.com>
2024-09-08 10:44:12 -0400

    Audio Saving

--- StarshipDestroyer/Assets/Scripts/Settings/VolumeController.cs
+++ StarshipDestroyer/Assets/Scripts/Settings/VolumeController.cs
@@ -9,14 +9,21 @@ public class VolumeController : MonoBehaviour
     [SerializeField] private Slider volumeSlider = null;
     [SerializeField] private TMP_Text volumeTextUI = null;
     [SerializeField] private string targetSoundName = "MenuMusic"; // Specify the sound name here
+    private const string volumeKeyPrefix = "Volume_";
 
     private void Start()
     {
-        // Initialize the slider with the current volume for the specific sound
-        float currentVolume = AudioManagerScript.instance.GetVolume(targetSoundName);
-        volumeSlider.value = currentVolume;
-        volumeTextUI.text = currentVolume.ToString("0.0");
-        
+        // Load the saved volume from PlayerPrefs
+        float savedVolume = PlayerPrefs.GetFloat(volumeKeyPrefix + targetSoundName, 1f); // Default volume is 1
+        volumeSlider.value = savedVolume;
+        volumeTextUI.text = savedVolume.ToString("0.0");
+
+        // Initialize the volume in the AudioManagerScript
+        if (AudioManagerScript.instance != null)
+        {
+            AudioManagerScript.instance.SetVolume(targetSoundName, savedVolume);
+        }
+
         // Add a listener to the slider to call the SetVolume method on change
         volumeSlider.onValueChanged.AddListener(SetVolume);
     }
@@ -25,11 +32,15 @@ public class VolumeController : MonoBehaviour
     {
         // Update the volume text
         volumeTextUI.text = volume.ToString("0.0");
-        
+
         // Update the volume in the AudioManagerScript for the specific sound
         if (AudioManagerScript.instance != null)
         {
             AudioManagerScript.instance.SetVolume(targetSoundName, volume);
         }
+
+        // Save the volume setting to PlayerPrefs
+        PlayerPrefs.SetFloat(volumeKeyPrefix + targetSoundName, volume);
+        PlayerPrefs.Save(); // Make sure to save the PlayerPrefs data
     }
 }

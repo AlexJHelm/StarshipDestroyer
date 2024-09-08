fd0171c9cb60b755f8dfcab86ad5bcf02a781300
Zurius <jacobsouza32@gmail.com>
2024-09-08 12:47:40 -0400

    Credits

---
+++ StarshipDestroyer/Assets/Scripts/Settings/AlexPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AlexPanel : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject AlexPanel1;
+    public GameObject DerekPanel2;
+    public GameObject JacobPanel3;
+    public GameObject KylePanel4;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AlexPanel1.SetActive(true);
+        DerekPanel2.SetActive(false);
+        JacobPanel3.SetActive(false);
+        KylePanel4.SetActive(false);
+
+    }
+}

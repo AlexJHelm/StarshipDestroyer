d41b3fb96b86898ffeaa2e957a9fbaa3d25aaf07
AlexJHelm <ajhelm@email.neit.edu>
2024-09-10 15:01:21 -0400

    Added New Ally Capitol Ship

--- StarshipDestroyer/Assets/Scripts/Capitol Ship/CollisionScript.cs
+++ StarshipDestroyer/Assets/Scripts/Capitol Ship/CollisionScript.cs
@@ -9,7 +9,7 @@ public class CollisionScript : MonoBehaviour
         Debug.Log("Entered");
         if (collision.gameObject.tag == "CapitolShip" || collision.gameObject.tag == "Asteroid" || collision.gameObject.tag == "Asteroid2" || collision.gameObject.tag == "Asteroid3" ||
             collision.gameObject.tag == "EnemyBridge" || collision.gameObject.tag == "EnemyWeapons" || collision.gameObject.tag == "EnemyThrusters" || collision.gameObject.tag == "AllyBridge" ||
-            collision.gameObject.tag == "AllyyWeapons" || collision.gameObject.tag == "AllyThrusters")
+            collision.gameObject.tag == "AllyWeapons" || collision.gameObject.tag == "AllyThrusters")
         { 
             if(this.tag == "PlayerLaser" || this.tag == "PlayerBomb" || this.tag == "Playermissile")
             {
